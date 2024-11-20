79d8db35b20aaf040a1eb1c84e2dc8c9be4aaf70
Aidan Nelson <6486359+AidanNelson@users.noreply.github.com>
2024-11-20 12:41:40 -0500

    init

---
+++ server.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+
+const app = express();
+const port = 3535;
+
+
+
+
+// Serve static files from the /client directory
+app.use(express.static(path.join(__dirname, 'client')));
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// We will use the socket.io library to manage Websocket connections
+const io = require("socket.io")().listen(server);
+
+let projectStatusSubscriptions = {};
+
+// Set up each socket connection
+io.on("connection", (socket) => {
+    console.log("New socket connected!");
+
+    socket.on('subscribe', (data) => {
+        console.log(`Subscribing to ${data.filePath}`);
+        projectStatusSubscriptions[socket.id] = data.filePath;
+    }
+
+});
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, 'uploads'));
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully');
+    projectStatus[req.file.path] = { uploadStatus: "uploaded" };
+    console.log('File uploaded, starting to process data w/ nerfstudio');
+    processUploadedVideoFile(req.file.path);
+});
+
+function processUploadedVideoFile(filePath) {
+    // make a directory under 'nerfstudio-output' to store data (using date.now() and file name as name)
+    const outputDir = path.join(__dirname, 'nerfstudio-output', `${Date.now()}-${path.basename(filePath)}`);
+    fs.mkdirSync(outputDir, { recursive: true });
+
+    // Create 'processed' directory under the output directory
+    const processedDir = path.join(outputDir, 'processed');
+    fs.mkdirSync(processedDir, { recursive: true });
+
+    // Create 'output' directory under the output directory
+    const outputDirPath = path.join(outputDir, 'output');
+    fs.mkdirSync(outputDirPath, { recursive: true });
+
+
+    let cmd = `ns-process-data video --data ${filePath}`;
+    cmd += ` --output-dir ${processedDir}`;
+    projectStatus[filePath]['stdout'] = "";
+
+
+    let nsProcessDataProcess = exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing ns-process-data: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`ns-process-data stderr: ${stderr}`);
+            return;
+        }
+        console.log(`ns-process-data output: ${stdout}`);
+    });
+
+    nsProcessDataProcess.stdout.on('data', (data) => {
+        console.log(`ns-train stdout: ${data}`);
+        for (let id in projectStatusSubscriptions) {
+            if (projectStatusSubscriptions[id] === filePath) {
+                io.to(id).emit('statusUpdate', data);
+            }
+        }
+    });
+
+    nsProcessDataProcess.on('close', (code) => {
+        console.log(`ns-process-data process exited with code ${code}`);
+        trainSplatfactoModel(processedDir, outputDir);
+    })
+}
+
+function trainSplatfactoModel(processedDir, outputDirPath) {
+
+    let cmd = `ns-train splatfacto --data ${processedDir}`;
+    cmd += ` --output-dir ${outputDirPath}`;
+
+    let nsTrainProcess = exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing ns-train: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`ns-train stderr: ${stderr}`);
+            return;
+        }
+        console.log(`ns-train output: ${stdout}`);
+    });
+
+    nsTrainProcess.stdout.on('data', (data) => {
+        console.log(`ns-train stdout: ${data}`);
+        for (let id in projectStatusSubscriptions) {
+            if (projectStatusSubscriptions[id] === filePath) {
+                io.to(id).emit('statusUpdate', data);
+            }
+        }
+    });
+
+    nsTrainProcess.on('close', (code) => {
+        console.log(`ns-train process exited with code ${code}`);
+
+    });
+}
+
